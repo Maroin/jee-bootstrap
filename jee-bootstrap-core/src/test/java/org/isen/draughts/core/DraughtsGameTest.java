81e422bbcd3fdc06d460e7101a998c5b30dbde5d
charles <charles.cartiaux@gmail.com>
2017-01-09 14:27:46 +0100

    create test File

---
+++ jee-bootstrap-core/src/test/java/org/isen/draughts/core/DraughtsGameTest.java
@@ -0,0 +1,22 @@
+package org.isen.draughts.core;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by charles on 10/01/2016.
+ */
+public class DraughtsGameTest {
+
+
+    @Before
+    public void doBefore() throws Exception {
+
+    }
+
+    @Test
+    public void aPlayerCanMove() throws Exception {
+
+    }
+
+}
