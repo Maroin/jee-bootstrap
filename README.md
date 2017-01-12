ea1251dce03172f84c60163b9bf4bc8a79274536
charles <charles.cartiaux@gmail.com>
2017-01-12 14:57:49 +0100

    clear git

--- README.md
+++
@@ -1,3 +0,0 @@
-# JEE Bootstrap
-
-Simple boostrap project to setup a multi-module maven project with some dependencies for JEE
--- jee-bootstrap-core/src/main/java/org/isen/draughts/core/Player.java
+++
@@ -1,8 +0,0 @@
-package org.isen.draughts.core;
-
-/**
- * Created by isen on 09/01/2017.
- */
-public enum Player {
-    WHITE, BLACK
-}
