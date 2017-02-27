3072e4cbcfecd54f03ac2eb1891a0323e7505ee9
Maroin <maroin.aldandachi@gmail.com>
2017-02-27 12:36:38 +0100

    itCanBrowseIndex test passed

--- jee-draughts-web/src/test/java/org.isen.draughts.web/DeploymentTest.java
+++ jee-draughts-web/src/test/java/org.isen.draughts.web/DeploymentTest.java
@@ -14,6 +14,7 @@ public class DeploymentTest extends JettyHarness {
     public void itCanBrowseIndex() throws Exception {
 
         String index = get(getBaseUri());
+        System.out.println(index);
         assertTrue(index.contains("Hello"));
     }
 }
--- jee-draughts-web/src/test/java/org.isen.draughts.web/JettyHarness.java
+++ jee-draughts-web/src/test/java/org.isen.draughts.web/JettyHarness.java
@@ -5,6 +5,7 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -15,8 +16,8 @@ import java.io.IOException;
 
 public class JettyHarness {
 
-    private static final int BASE_PORT = 9090;
-    private static final int MAX_PORT_DEVIATION = 0;
+    private static final int BASE_PORT = 63342;
+    private static final int MAX_PORT_DEVIATION = 100;
     private static final String CONTEXT_PATH = "";
 
     private HttpClient httpClient;
@@ -26,10 +27,15 @@ public class JettyHarness {
             + (int) (Math.random() * MAX_PORT_DEVIATION);
 
 
+    protected static final String HELLO_SERVLET_PATH = "/hello";
+    protected static final String HELLO_SERVLET_URI = getBaseUri()
+            + HELLO_SERVLET_PATH;
+
     @BeforeClass
     public static void startServer() throws Exception {
         server = new Server(port);
         server.setStopAtShutdown(true);
+
 
         WebAppContext context = new WebAppContext();
 
@@ -38,11 +44,18 @@ public class JettyHarness {
         context.setDescriptor(wardir + "WEB-INF/web.xml");
 
         context.setContextPath("/" + CONTEXT_PATH);
+        context.setParentLoaderPriority(true);
+        context.addServlet(new ServletHolder(new HelloServlet()),
+                HELLO_SERVLET_PATH);
+       server.setHandler(context);
 
-        server.setHandler(context);
-        server.start();
-
-
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        //server.join();
     }
 
 
